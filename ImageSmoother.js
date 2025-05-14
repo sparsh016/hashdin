e0f8a78908a67456ec8c034839f2bfb0791acf33
Anubhav kumar <128998627+sparsh016@users.noreply.github.com>
2025-05-14 05:56:39 +0530

    Update ImageSmoother.js

--- ImageSmoother.js
+++ ImageSmoother.js
@@ -5,7 +5,7 @@ function ImageSmoother() {
   const smoothedCanvasRef = useRef(null);
   const [pixelInfo, setPixelInfo] = useState('');
   const [grayscale, setGrayscale] = useState(false);
-  const [size, setSize] = useState(3);
+  const [neighborhoodSize, setNeighborhoodSize] = useState(3);
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
@@ -13,35 +13,34 @@ function ImageSmoother() {
     const img = new Image();
     img.onload = () => {
       ctx.drawImage(img, 0, 0, 300, 300);
-      if (grayscale) convertToGrayscale(ctx);
-      smoothImage();
+      if (grayscale) applyGrayscale(ctx);
+      applySmoothing();
     };
     img.src = URL.createObjectURL(file);
   };
 
-  const convertToGrayscale = (ctx) => {
+  const applyGrayscale = (ctx) => {
     const imageData = ctx.getImageData(0, 0, 300, 300);
     const data = imageData.data;
     for (let i = 0; i < data.length; i += 4) {
-      const avg = 0.299 * data[i] + 0.587 * data[i+1] + 0.114 * data[i+2];
-      data[i] = data[i+1] = data[i+2] = avg;
+      const avg = 0.299 * data[i] + 0.587 * data[i + 1] + 0.114 * data[i + 2];
+      data[i] = data[i + 1] = data[i + 2] = avg;
     }
     ctx.putImageData(imageData, 0, 0);
   };
 
-  const smoothImage = () => {
+  const applySmoothing = () => {
     const oCtx = originalCanvasRef.current.getContext('2d');
     const sCtx = smoothedCanvasRef.current.getContext('2d');
     const imageData = oCtx.getImageData(0, 0, 300, 300);
     const output = sCtx.createImageData(imageData);
     const { data } = imageData;
     const outData = output.data;
-    const sizeHalf = Math.floor(size / 2);
+    const sizeHalf = Math.floor(neighborhoodSize / 2);
 
     for (let y = 0; y < 300; y++) {
       for (let x = 0; x < 300; x++) {
         let r = 0, g = 0, b = 0, a = 0, count = 0;
-
         for (let dy = -sizeHalf; dy <= sizeHalf; dy++) {
           for (let dx = -sizeHalf; dx <= sizeHalf; dx++) {
             const ny = y + dy;
@@ -56,7 +55,6 @@ function ImageSmoother() {
             }
           }
         }
-
         const i = (y * 300 + x) * 4;
         outData[i] = r / count;
         outData[i + 1] = g / count;
@@ -68,7 +66,7 @@ function ImageSmoother() {
     sCtx.putImageData(output, 0, 0);
   };
 
-  const handleHover = (e) => {
+  const showPixelInfo = (e) => {
     const rect = originalCanvasRef.current.getBoundingClientRect();
     const x = Math.floor(e.clientX - rect.left);
     const y = Math.floor(e.clientY - rect.top);
@@ -80,23 +78,23 @@ function ImageSmoother() {
   return (
     <div>
       <input type="file" onChange={handleImageUpload} accept="image/*" />
-      <br />
+      <br /><br />
       <label>
         <input type="checkbox" checked={grayscale} onChange={() => setGrayscale(!grayscale)} />
-        Convert to Grayscale
+        Convert to Grayscale First
       </label>
-      <br />
+      <br /><br />
       <label>
-        Neighborhood Size:
-        <select value={size} onChange={(e) => setSize(parseInt(e.target.value))}>
+        Neighborhood Size:&nbsp;
+        <select value={neighborhoodSize} onChange={(e) => setNeighborhoodSize(parseInt(e.target.value))}>
           <option value={3}>3x3</option>
           <option value={5}>5x5</option>
         </select>
       </label>
       <br /><br />
-      <canvas ref={originalCanvasRef} width="300" height="300" onMouseMove={handleHover} />
+      <canvas ref={originalCanvasRef} width="300" height="300" onMouseMove={showPixelInfo} />
       <canvas ref={smoothedCanvasRef} width="300" height="300" />
-      <div>{pixelInfo}</div>
+      <div style={{ marginTop: '10px', fontWeight: 'bold' }}>{pixelInfo}</div>
     </div>
   );
 }
